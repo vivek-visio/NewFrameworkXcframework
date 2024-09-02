6ecd216ce7ab224778c08cd1a553b72aecb8a44b
Vivek <vivek@visio-apps.com>
2024-09-02 14:58:42 +0530

    finishmy framework

---
+++ NewFrameworkXcframework/NewFrameworkXcframework/NewFrameworkXcframework.h
@@ -0,0 +1,18 @@
+//
+//  NewFrameworkXcframework.h
+//  NewFrameworkXcframework
+//
+//  Created by Visioapps on 02/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NewFrameworkXcframework.
+FOUNDATION_EXPORT double NewFrameworkXcframeworkVersionNumber;
+
+//! Project version string for NewFrameworkXcframework.
+FOUNDATION_EXPORT const unsigned char NewFrameworkXcframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewFrameworkXcframework/PublicHeader.h>
+
+
